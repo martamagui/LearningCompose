5a9c5f5433861bffc5e5f68f6df3d824e353ee97
MartaMagui <marta.molina@live.u-tad.com>
2022-12-29 20:11:20 +0100

    States pt. 1

--- UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/MainActivity.kt
+++ UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,6 +38,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun MyStateExample() {
+    //remember almacena el estado anterior de la vista al repintarse
+    var counter = remember { mutableStateOf(0) }
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter.value += 1 }) {
+            Text(text = "Pulsar")
+        }
+        Text(text = "He sido pulsado ${counter.value} veces")
+
+    }
+}
+
 @Preview(
     name = "Preview nº1",
     showBackground = true,
@@ -42,6 +62,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     UdemyCourseDummyProjectTheme {
-        MyComplexLayout()
+        MyStateExample()
     }
 }
