2609f12eda85ce5bb3d611dab96d88b1493c4819
MartaMagui <marta.molina@live.u-tad.com>
2022-12-29 19:37:32 +0100

    Combining Layouts

--- UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/MainActivity.kt
+++ UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.udemycoursedummyproject.theory.MyBox
+import com.example.udemycoursedummyproject.theory.MyRow
 import com.example.udemycoursedummyproject.ui.theme.UdemyCourseDummyProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,123 +38,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyRow() {
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .horizontalScroll(rememberScrollState())
-    ) {
-        Text(
-            text = "Ejemplo", modifier = Modifier
-                .padding(20.dp)
-        )
-        Text(
-            text = "Ejemplo", modifier = Modifier
-                .padding(20.dp)
-        )
-        Text(
-            text = "Ejemplo", modifier = Modifier
-                .padding(20.dp)
-        )
-        Text(
-            text = "Ejemplo", modifier = Modifier
-                .padding(20.dp)
-        )
-        Text(
-            text = "Ejemplo", modifier = Modifier
-                .padding(20.dp)
-        )
-        Text(
-            text = "Ejemplo", modifier = Modifier
-                .padding(20.dp)
-        )
-
-    }
-}
-
-@Composable
-fun MyColumn() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(
-                rememberScrollState()
-            ),
-        verticalArrangement = Arrangement.SpaceEvenly,
-
-        ) {
-        Text(
-            text = "hola", modifier = Modifier
-                .background(Color.Cyan)
-                .fillMaxWidth()
-                .height(300.dp)
-        )
-        Text(
-            text = "hola", modifier = Modifier
-                .background(Color.Magenta)
-                .fillMaxWidth()
-                .height(300.dp)
-        )
-        Text(
-            text = "hola", modifier = Modifier
-                .background(Color.Green)
-                .fillMaxWidth()
-                .height(300.dp)
-        )
-        Text(
-            text = "hola", modifier = Modifier
-                .background(Color.Cyan)
-                .fillMaxWidth()
-                .height(300.dp)
-        )
-        Text(
-            text = "hola", modifier = Modifier
-                .background(Color.Magenta)
-                .fillMaxWidth()
-                .height(30.dp)
-        )
-        Text(
-            text = "hola", modifier = Modifier
-                .background(Color.Green)
-                .fillMaxWidth()
-                .height(30.dp)
-        )
-        Text(
-            text = "hola", modifier = Modifier
-                .background(Color.Cyan)
-                .fillMaxWidth()
-                .height(30.dp)
-        )
-        Text(text = "hola", modifier = Modifier.background(Color.Magenta))
-        Text(text = "hola", modifier = Modifier.background(Color.Green))
-        /*  Text(text = "hola", modifier = Modifier.background(Color.Cyan).weight(3f))
-            Text(text = "hola", modifier = Modifier.background(Color.Magenta).weight(1f))
-            Text(text = "hola", modifier = Modifier.background(Color.Green).weight(1f))
-        */
-    }
-}
-
-@Composable
-fun MyBox(name: String) {
-    //Si aun box no se le asigna altura o anchura,
-    // va a ocupar unicamente el espacio que necesite
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
+fun MyComplexLayout() {
+    Column(Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
-                .width(70.dp)
-                .height(50.dp)
-                .background(Color.Cyan)
-                //Para que haya scroll
-                .verticalScroll(
-                    rememberScrollState()
-                ),
-            contentAlignment = Alignment.Center
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.LightGray)
         ) {
-            Text(text = "Esto es un texto largo el cual se podrá scrollear en $name")
         }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.Yellow)
+            ) {
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.DarkGray),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "This is a text in a box",
+                    modifier = Modifier
+                        .background(Color.White)
+                        .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Blue)
+        ) {
+        }
+
     }
 }
 
@@ -163,8 +92,6 @@ fun MyBox(name: String) {
 @Composable
 fun DefaultPreview() {
     UdemyCourseDummyProjectTheme {
-        // MyBox("Android")
-        //MyColumn()
-        MyRow()
+        MyComplexLayout()
     }
 }
---
+++ UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/theory/LayaoutTheory.kt
@@ -0,0 +1,151 @@
+package com.example.udemycoursedummyproject.theory
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.udemycoursedummyproject.ui.theme.UdemyCourseDummyProjectTheme
+
+@Composable
+fun MyRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = "Ejemplo", modifier = Modifier
+                .padding(20.dp)
+        )
+        Text(
+            text = "Ejemplo", modifier = Modifier
+                .padding(20.dp)
+        )
+        Text(
+            text = "Ejemplo", modifier = Modifier
+                .padding(20.dp)
+        )
+        Text(
+            text = "Ejemplo", modifier = Modifier
+                .padding(20.dp)
+        )
+        Text(
+            text = "Ejemplo", modifier = Modifier
+                .padding(20.dp)
+        )
+        Text(
+            text = "Ejemplo", modifier = Modifier
+                .padding(20.dp)
+        )
+
+    }
+}
+
+@Composable
+fun MyColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(
+                rememberScrollState()
+            ),
+        verticalArrangement = Arrangement.SpaceEvenly,
+
+        ) {
+        Text(
+            text = "hola", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(300.dp)
+        )
+        Text(
+            text = "hola", modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(300.dp)
+        )
+        Text(
+            text = "hola", modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+                .height(300.dp)
+        )
+        Text(
+            text = "hola", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(300.dp)
+        )
+        Text(
+            text = "hola", modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(30.dp)
+        )
+        Text(
+            text = "hola", modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+                .height(30.dp)
+        )
+        Text(
+            text = "hola", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(30.dp)
+        )
+        Text(text = "hola", modifier = Modifier.background(Color.Magenta))
+        Text(text = "hola", modifier = Modifier.background(Color.Green))
+        /*  Text(text = "hola", modifier = Modifier.background(Color.Cyan).weight(3f))
+            Text(text = "hola", modifier = Modifier.background(Color.Magenta).weight(1f))
+            Text(text = "hola", modifier = Modifier.background(Color.Green).weight(1f))
+        */
+    }
+}
+
+@Composable
+fun MyBox(name: String) {
+    //Si aun box no se le asigna altura o anchura,
+    // va a ocupar unicamente el espacio que necesite
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .width(70.dp)
+                .height(50.dp)
+                .background(Color.Cyan)
+                //Para que haya scroll
+                .verticalScroll(
+                    rememberScrollState()
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Esto es un texto largo el cual se podrá scrollear en $name")
+        }
+    }
+}
+
+
+/*
+@Preview(
+    name = "Preview nº1",
+    showBackground = true,
+)
+@Composable
+fun DefaultPreview() {
+    UdemyCourseDummyProjectTheme {
+        // MyBox("Android")
+        //MyColumn()
+        MyRow()
+    }
+}*/
