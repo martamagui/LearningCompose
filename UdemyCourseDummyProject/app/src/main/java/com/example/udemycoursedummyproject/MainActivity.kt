15a811260c72b858c040cc4787e922b4b75468f1
MartaMagui <marta.molina@live.u-tad.com>
2022-12-30 19:04:33 +0100

    Components: Buttons with state

--- UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/MainActivity.kt
+++ UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,13 +40,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyButton() {
+
+    var enabled by rememberSaveable { mutableStateOf(true) }
+
     Column(
         Modifier
             .fillMaxSize()
             .padding(24.dp)
     ) {
         Button(
-            onClick = { Log.i("My button", "CLICK!") },
+            onClick = {
+                Log.i("My button", "CLICK!")
+                enabled != enabled
+            },
+            enabled = enabled,
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = Color.Magenta,
                 contentColor = Color.Blue
