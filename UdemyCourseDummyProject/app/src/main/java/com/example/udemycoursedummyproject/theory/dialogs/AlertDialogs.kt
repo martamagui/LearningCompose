fc606a237717448adc713d96c027d35c8a151d62
Marta Molina <marta.molina@live.u-tad.com>
2023-01-09 22:08:07 +0100

    AdvancedCustomDialog

--- UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/theory/dialogs/AlertDialogs.kt
+++ UdemyCourseDummyProject/app/src/main/java/com/example/udemycoursedummyproject/theory/dialogs/AlertDialogs.kt
@@ -1,8 +1,11 @@
 package com.example.udemycoursedummyproject.theory.dialogs
 
 import android.util.Log
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -13,10 +16,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.example.udemycoursedummyproject.R
 
 @Composable
 fun MyAlertDialogContainer() {
@@ -36,7 +45,8 @@ fun MyAlertDialogContainer() {
             onDismiss = { isShown = false },
             onConfirm = { Log.i("MyAlertDialog", "Confirmado") }
         )
-        MyCustomDialog(show = isShown) { isShown = false }
+        MyCustomDialog(show = false) { isShown = false }
+        MyAdvancedCustomDialog(isShown) { isShown = true }
     }
 }
 
@@ -77,7 +87,9 @@ fun MyCustomDialog(
 ) {
     if (show) {
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
             contentAlignment = Alignment.Center
         ) {
             //Deja crear lo que queramos
@@ -100,4 +112,65 @@ fun MyCustomDialog(
             }
         }
     }
+}
+
+
+@Composable
+fun MyAdvancedCustomDialog(
+    show: Boolean,
+    onDismiss: () -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        if (show) {
+            Dialog(
+                onDismissRequest = { onDismiss() },
+                properties = DialogProperties(
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true
+                )
+            ) {
+                Column(
+                    Modifier
+                        .background(Color.White)
+                        .padding(12.dp)
+                        .fillMaxWidth()
+                ) {
+                    MyTitleDialog("Set backup account")
+                    AccountItem(email = "mail@mail.com", drawable = R.drawable.pic_one)
+                    AccountItem(email = "mail2@mail.com", drawable = R.drawable.pic_two)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AccountItem(email: String, @DrawableRes drawable: Int) {
+    Row(
+        Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = drawable), contentDescription = "avatar",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Text(text = email)
+    }
+}
+
+@Composable
+fun MyTitleDialog(title: String) {
+    Text(
+        text = title,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(bottom = 12.dp)
+    )
 }
